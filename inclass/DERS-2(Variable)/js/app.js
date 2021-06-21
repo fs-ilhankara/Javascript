94d78d53b257692fd532ce9e0fe28f847507e6b6
mirackarakasli <ilhansamikara@gmail.com>
2021-06-21 20:11:28 +0300

    js-3

---
+++ inclass/DERS-2(Variable)/js/app.js
@@ -0,0 +1,26 @@
+
+// console.time();
+// console.log("hello clarusway");
+// console.info('some info');
+// console.warn('some warning');
+// console.error('error!!!');
+
+// const PI = 3.1415;
+
+// let myNum = 5;
+
+// console.log(PI);
+
+// myNumA = 3;
+
+// console.log(myNum);
+// console.log(myNumA);
+
+let num1 = 42;
+
+// if (num1 == 42) {
+    //     // var num2 = 22;
+    //     console.log(num1, num2);
+    // }
+console.log(num1, num2);
+var num2 ;
